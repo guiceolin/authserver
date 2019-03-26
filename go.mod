58f3c0b69ae3a7f20d39fbc53d9934f6c8691403
Guilherme Ceolin <guiceolin@gmail.com>
2019-03-26 19:56:49 -0300

    It Works! MUAHAHAHAHA

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.12
 
 require (
 	github.com/astaxie/beego v1.11.1
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible
 	github.com/julienschmidt/httprouter v1.2.0
 	github.com/lib/pq v1.0.0
 	google.golang.org/appengine v1.5.0 // indirect
--- main.go
+++ main.go
@@ -5,8 +5,11 @@ import (
 	"html/template"
 	"log"
 	"net/http"
+	"net/http/httputil"
+	"time"
 
 	"github.com/astaxie/beego/orm"
+	jwt "github.com/dgrijalva/jwt-go"
 	_ "github.com/lib/pq"
 
 	"github.com/julienschmidt/httprouter"
@@ -15,6 +18,7 @@ import (
 type server struct {
 	router *httprouter.Router
 	db     orm.Ormer
+	tmpl   *template.Template
 }
 
 type User struct {
@@ -26,16 +30,96 @@ type User struct {
 
 func (s *server) routes() {
 	s.router.HandlerFunc("GET", "/", s.handleIndex())
+	s.router.HandlerFunc("GET", "/sessions/new", s.handleNewSession())
+	s.router.HandlerFunc("POST", "/sessions", s.handleCreateSession())
+}
+
+func (s *server) handleCreateSession() http.HandlerFunc {
+	type Credentials struct {
+		Password string `json:"password"`
+		Email    string `json:"email"`
+	}
+
+	type Claims struct {
+		Id int `json:"id"`
+		jwt.StandardClaims
+	}
+	return func(w http.ResponseWriter, r *http.Request) {
+
+		requestDump, err := httputil.DumpRequest(r, true)
+		if err != nil {
+			fmt.Println(err)
+		}
+		fmt.Println(string(requestDump))
+
+		r.ParseForm()
+		creds := Credentials{
+			Email:    r.FormValue("email"),
+			Password: r.FormValue("password"),
+		}
+
+		user := User{Email: creds.Email}
+		err = s.db.Read(&user, "Email")
+		if err != nil {
+			// If the structure of the body is wrong, return an HTTP error
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+
+		if user.Password != creds.Password {
+			w.WriteHeader(http.StatusUnauthorized)
+			return
+		}
+
+		var jwtKey = []byte("my_secret_key")
+
+		// Declare the expiration time of the token
+		// here, we have kept it as 5 minutes
+		expirationTime := time.Now().Add(5 * time.Minute)
+		// Create the JWT claims, which includes the username and expiry time
+		claims := &Claims{
+			Id: user.Id,
+			StandardClaims: jwt.StandardClaims{
+				// In JWT, the expiry time is expressed as unix milliseconds
+				ExpiresAt: expirationTime.Unix(),
+			},
+		}
+
+		// Declare the token with the algorithm used for signing, and the claims
+		token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+		// Create the JWT string
+		tokenString, err := token.SignedString(jwtKey)
+		if err != nil {
+			// If there is an error in creating the JWT return an internal server error
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+
+		// Finally, we set the client cookie for "token" as the JWT we just generated
+		// we also set an expiry time which is the same as the token itself
+		http.SetCookie(w, &http.Cookie{
+			Name:    "token",
+			Value:   tokenString,
+			Expires: expirationTime,
+			Domain:  "localhost",
+		})
+
+	}
+}
+
+func (s *server) handleNewSession() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		s.tmpl.ExecuteTemplate(w, "sessions/new", nil)
+
+	}
 }
 
 func (s *server) handleIndex() http.HandlerFunc {
-	tmpl := template.Must(template.ParseFiles("./templates/index.html"))
 	data := struct {
 		CurrentUser *User
 	}{}
 	return func(w http.ResponseWriter, r *http.Request) {
-		fmt.Println(data)
-		tmpl.Execute(w, data)
+		s.tmpl.ExecuteTemplate(w, "index", data)
 	}
 }
 
@@ -47,9 +131,14 @@ func init() {
 }
 
 func main() {
+	tmpl, err := template.ParseGlob("templates/**/*")
+	if err != nil {
+		log.Fatal(err)
+	}
 	s := server{
 		router: httprouter.New(),
-		db:     orm.NewOrm()}
+		db:     orm.NewOrm(),
+		tmpl:   tmpl}
 	s.routes()
 
 	log.Fatal(http.ListenAndServe(":8080", s.router))
