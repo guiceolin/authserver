2caeb0fa54e1ebcd4280d3478b77b58173abb200
Guilherme Ceolin <guiceolin@gmail.com>
2019-03-28 21:51:58 -0300

    Extract jwt token built to func

--- main.go
+++ main.go
@@ -22,10 +22,30 @@ type server struct {
 }
 
 type User struct {
-	Id       int    `orm:"auto"`
-	Name     string `orm:"size(100)"`
-	Email    string `orm:"unique"`
-	Password string
+	Id       int    `orm:"auto" json:"id"`
+	Name     string `orm:"size(100)" json:"name"`
+	Email    string `orm:"unique" json:"email"`
+	Password string `json:"-"`
+}
+
+func buildJWT(payload interface{}, expirationTime time.Time) (string, error) {
+	type Claims struct {
+		Payload interface{} `json:"payload"`
+		jwt.StandardClaims
+	}
+
+	var jwtKey = []byte(viper.GetString("jwt_secret"))
+	claims := &Claims{
+		Payload: payload,
+		StandardClaims: jwt.StandardClaims{
+			ExpiresAt: expirationTime.Unix(),
+		},
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	tokenString, err := token.SignedString(jwtKey)
+	return tokenString, err
+
 }
 
 func (s *server) routes() {
@@ -39,11 +59,8 @@ func (s *server) handleCreateSession() http.HandlerFunc {
 		Password string `json:"password"`
 		Email    string `json:"email"`
 	}
+	var expirationTime = time.Now().Add(5 * time.Minute)
 
-	type Claims struct {
-		Id int `json:"id"`
-		jwt.StandardClaims
-	}
 	return func(w http.ResponseWriter, r *http.Request) {
 		logger.LogRequest(r)
 
@@ -54,7 +71,7 @@ func (s *server) handleCreateSession() http.HandlerFunc {
 		}
 
 		user := User{Email: creds.Email}
-		err = s.db.Read(&user, "Email")
+		err := s.db.Read(&user, "Email")
 		if err != nil {
 			// If the structure of the body is wrong, return an HTTP error
 			w.WriteHeader(http.StatusBadRequest)
@@ -66,32 +83,12 @@ func (s *server) handleCreateSession() http.HandlerFunc {
 			return
 		}
 
-		var jwtKey = []byte(viper.GetString("jwt_secret"))
-
-		// Declare the expiration time of the token
-		// here, we have kept it as 5 minutes
-		expirationTime := time.Now().Add(5 * time.Minute)
-		// Create the JWT claims, which includes the username and expiry time
-		claims := &Claims{
-			Id: user.Id,
-			StandardClaims: jwt.StandardClaims{
-				// In JWT, the expiry time is expressed as unix milliseconds
-				ExpiresAt: expirationTime.Unix(),
-			},
-		}
-
-		// Declare the token with the algorithm used for signing, and the claims
-		token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
-		// Create the JWT string
-		tokenString, err := token.SignedString(jwtKey)
+		tokenString, err := buildJWT(user, expirationTime)
 		if err != nil {
-			// If there is an error in creating the JWT return an internal server error
 			w.WriteHeader(http.StatusInternalServerError)
 			return
 		}
 
-		// Finally, we set the client cookie for "token" as the JWT we just generated
-		// we also set an expiry time which is the same as the token itself
 		http.SetCookie(w, &http.Cookie{
 			Name:    "token",
 			Value:   tokenString,
